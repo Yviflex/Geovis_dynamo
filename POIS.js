4cb5346b84d7d71125d1cf7ab2ff2577a2686655
Yviflex <122730275+Yviflex@users.noreply.github.com>
2024-01-30 23:47:34 +0100

    geodata

---
+++ POIS.js
@@ -0,0 +1,20 @@
+var pois = {
+"type": "FeatureCollection",
+"name": "POISdino",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "courtedoux", "nom": "Sentier didactique \"Sur les traces des géants\"","image": "XXXXX", "ville": "Courtedoux", "canton": "JU", "type": "Empreintes de Sauropodes", "no": 8, "temps": "Jurassique" }, "geometry": { "type": "Point", "coordinates": [ 7.02105, 47.40102 ] } },
+{ "type": "Feature", "properties": { "id": "frick", "nom": "Tongrube Gruhalde","image": "XXXXX", "ville": "Frick", "canton": "AG", "type": "Plateosaurus", "no": 1, "temps": "Trias" }, "geometry": { "type": "Point", "coordinates": [ 8.00876, 47.50659 ] } },
+{ "type": "Feature", "properties": { "id": "pizDalDiavel", "nom": "Piz dal Diavel","image": "XXXXX", "ville": "Zernez", "canton": "GR", "type": "Empreintes de Prosauropodes", "no": 5, "temps": "Trias" }, "geometry": { "type": "Point", "coordinates": [ 10.13956, 46.62248 ] } },
+{ "type": "Feature", "properties": { "id": "gorgesMoutier", "nom": "Gorges de Moutier","image": "XXXXX", "ville": "Moutier", "canton": "BE", "type": "Empreintes de Sauropodes", "no": 8, "temps": "Jurassique" }, "geometry": { "type": "Point", "coordinates": [ 7.38066, 47.29357 ] } },
+{ "type": "Feature", "properties": { "id": "beckenried", "nom": "Risletenschlucht","image": "XXXXX", "ville": "Beckenried", "canton": "NW", "type": "Empreintes d'Iguanodon", "no": 7, "temps": "Crétacé" }, "geometry": { "type": "Point", "coordinates": [ 8.5251, 46.96728 ] } },
+{ "type": "Feature", "properties": { "id": "lommiswil", "nom": "Steinbruch Lommiswil/Oberdorf","image": "XXXXX", "ville": "Lommiswil", "canton": "SO", "type": "Empreintes de Sauropodes", "no": 8, "temps": "Jurassique" }, "geometry": { "type": "Point", "coordinates": [ 7.47532, 47.23602 ] } },
+{ "type": "Feature", "properties": { "id": "oberbuchsiten", "nom": "Steinbruch STAG","image": "XXXXX", "ville": "Egerkingen", "canton": "SO", "type": "Stegosaurus", "no": 4, "temps": "Jurassique" }, "geometry": { "type": "Point", "coordinates": [ 7.77806, 47.32228 ] } },
+{ "type": "Feature", "properties": { "id": "roeschenz", "nom": "Silberlöcher","image": "XXXXX", "ville": "Röschenz", "canton": "BL", "type": "Theropode", "no": 2, "temps": "Jurassique" }, "geometry": { "type": "Point", "coordinates": [ 7.47615, 47.43831 ] } },
+{ "type": "Feature", "properties": { "id": "fuellinsdorf", "nom": "Niederschönthal","image": "XXXXX", "ville": "Füllinsdorf", "canton": "BL", "type": "Plateosaurus", "no": 1, "temps": "Trias" }, "geometry": { "type": "Point", "coordinates": [ 7.72626, 47.5018 ] } },
+{ "type": "Feature", "properties": { "id": "pizEla", "nom": "Piz Ela","image": "XXXXX", "ville": "Bergün Filisur", "canton": "GR", "type": "Empreintes de Theropodes", "no": 6, "temps": "Trias" }, "geometry": { "type": "Point", "coordinates": [ 9.70804, 46.6019 ] } },
+{ "type": "Feature", "properties": { "id": "rochefort", "nom": "Rochefort Les Grattes","image": "XXXXX", "ville": "Rochefort", "canton": "NE", "type": "Empreintes de Sauropodes", "no": 8, "temps": "Jurassique" }, "geometry": { "type": "Point", "coordinates": [ 6.81517, 46.98607 ] } },
+{ "type": "Feature", "properties": { "id": "carriereGolats", "nom": "Carrière des Golats","image": "XXXXX", "ville": "Moutier", "canton": "BE", "type": "Sauropode", "no": 3, "temps": "Jurassique" }, "geometry": { "type": "Point", "coordinates": [ 7.37705, 47.28889 ] } }
+]
+};
+    
